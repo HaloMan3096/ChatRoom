961891aee1a4be776a4a0cd3b90a46642c1455dc
logan <Loganold@hotmail.com>
2025-02-27 23:02:20 -0500

    created skeleton

--- chat_room/server.js
+++ chat_room/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+// Serve static files from the same directory where server.js is located
+app.use(express.static(__dirname));
+
+// Route to serve index.html
 app.get('/', (req, res) => {
-    res.send('Hello, AWS World!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+// Start server
+const PORT = 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
