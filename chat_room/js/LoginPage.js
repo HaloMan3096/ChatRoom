1f13404a40e978d9dae5338ff6c3b5e19da5aec6
logan <Loganold@hotmail.com>
2025-03-03 11:10:19 -0500

    oops

--- chat_room/js/LoginPage.js
+++ chat_room/js/LoginPage.js
@@ -9,14 +9,13 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email: email, password }), // Sending `email` instead of `username`
-            credentials: 'include'  // Important to send cookies!
+            credentials: 'include'
         });
 
         const data = await response.json();
         console.log('Server Response:', data);
 
         if (response.ok) {
-            alert("Login successful!");
             window.location.href = '../index.html';
         } else {
             alert(data.message || 'Login failed');
