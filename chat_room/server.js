97f10ab2ad079e59c44e5041152b2b7716856815
logan <Loganold@hotmail.com>
2025-03-03 12:11:21 -0500

    oops

--- chat_room/server.js
+++ chat_room/server.js
@@ -96,10 +96,9 @@ app.post('/login', async (req, res) => {
                 return res.status(400).json({ message: "Invalid email or password" });
             }
 
-            res.cookie('userId', user.id, { httpOnly: true, secure: true });
+            res.cookie('userId', user.id, { httpOnly: true, sameSite: 'Lax' });
 
             res.status(200).json({ message: 'Login successful', user });
-            res.cookie('userId', user.id, { httpOnly: true, sameSite: 'Lax' });
         });
 
     } catch (error) {
